7b391bdfecc882be9b68b82c23b8aa610b9779cf
matu.dev <mati.a.gimenez2002@gmail.com>
2025-05-29 22:13:02 -0300

    Clase 18

--- Clase-18/script.js
+++ Clase-18/script.js
@@ -1,40 +1,75 @@
-//Es de tipo objeto y a estos objetos les decimos diccionarios
-const ID_VALUES_COUNTER = {
-    INCREMENT_BTN: 'increment-counter-btn',
-    DECREMENT_BTN: 'decrement-counter-btn',
-    COUNTER: 'counter-value'
+const messages = [
+    {
+        id: 1,
+        author: 'jose',
+        text: 'hola',
+        date: '2021-10-10 12:00:00'
+    },
+    {
+        id: 2,
+        author: 'pedro',
+        text: 'Que tal?',
+        date: '2021-10-10 12:00:00'
+    },
+    {
+        id: 3,
+        author: 'jose',
+        text: 'bien',
+        date: '2021-10-10 12:00:00'
+    }
+]
+
+const ID_MESSAGES_VALUES = {
+    MESSAGES_CONTAINER: 'messages-container',
+    MESSAGE_FORM: 'message-form'
 }
 
-const increment_btn_HTML = document.getElementById(ID_VALUES_COUNTER.INCREMENT_BTN)
-const decrement_btn_HTML = document.getElementById(ID_VALUES_COUNTER.DECREMENT_BTN)
-const counter_HTML = document.getElementById(ID_VALUES_COUNTER.COUNTER)
+const messages_container_HTML = document.getElementById(ID_MESSAGES_VALUES.MESSAGES_CONTAINER)
+const message_form_HTML = document.getElementById(ID_MESSAGES_VALUES.MESSAGE_FORM)
 
-//Necesito la variable que guarde el valor de mi contador (empieza en 0)
-let counter = 20
-
-
-function renderCounter () {
-    counter_HTML.innerText = counter
+function renderMessages () {
+    //Logica para renderizar la lista de mensajes
+    let messages_html_string = ''
+    for(const message of messages){
+        messages_html_string = messages_html_string + `
+            <div>
+                <h3>${message.author}</h3>
+                <p>${message.text}</p>
+                <span>Fecha: ${message.date}</span>
+                <hr/>
+            </div>
+        ` 
+    }
+    messages_container_HTML.innerHTML = messages_html_string
 }
 
-function incrementCounter () {
-    counter = counter + 1
-    renderCounter()
+function addNewMessage (new_message){
+    messages.push(new_message)
+    renderMessages()
 }
 
-function decrementCounter () {
-    counter = counter - 1
-    renderCounter()
-}
+//El primer parametro asociado a un evento recibira por parametro un objeto llamado event
+//Ese parametro event lo genera el navegador
+function handleSubmitMessageForm (event){
+    //Esto previene el comportamiento por defecto del evento submit
+    event.preventDefault()
 
-//cada vez que el usuario incremente o decremente el contador yo volvere a imprimir el resultado en la pantalla
+    //Quiero capturar el mensaje de mi input dentro del formulario
+    let message_text = message_form_HTML.message.value
+    const new_message = {
+        author: 'jose',
+        text: message_text,
+        date: '2021-10-10 12:00:00'
+    }
+    addNewMessage(new_message)
 
-//Cuando le de click a '+' incrementara
-//Cuando le de click a '-' decrementara
-increment_btn_HTML.addEventListener('click', incrementCounter)
-decrement_btn_HTML.addEventListener('click', decrementCounter)
+    //Reseteamos el input a vacio nuevamente
+    message_form_HTML.message.value = ''
+} 
 
-//la primera vez que se carge la pagina se renderize el contador
-renderCounter()
+//Evento para manejar el envio del formulario
+//El evento submit por defecto recarga la pagina
+message_form_HTML.addEventListener('submit', handleSubmitMessageForm)
 
-
+//Quiero que la primera vez que se ejecute el codigo se vea la lista de mensajes
+renderMessages()
